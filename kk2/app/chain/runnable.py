cad9a70dce33244cf45aa3226dc97e1f60b373b8
Nehaasati <31asatineha@gmail.com>
2026-05-30 15:27:53 +0200

    add runnable typevar

---
+++ kk2/app/chain/runnable.py
@@ -0,0 +1,7 @@
+from pydentic import Basemodel ,ConfigDict,SerilizerasAny
+from typing import Generic, TypeVar, Any, Callable
+
+# Type variable for INput output and inermidatevariable
+I = TypeVar("I")
+M = TypeVar("M")
+O = TypeVar("o")
